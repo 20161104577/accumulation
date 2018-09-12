199b550a6a619d16ccbbf077009b91dfe56d685c
杨大侠 <873233209@qq.com>
2018-09-12 11:38:41 +0800

    add
    1

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  accumulation
+//
+//  Created by 杨大侠 on 2018/9/12.
+//  Copyright © 2018年 杨大侠. All rights reserved.
+//
+
+import Foundation
+
+var sum = 0
+
+for index in 1...100 {
+    sum += index
+}
+
+print(sum)
